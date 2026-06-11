05beb32f9ad4c92800e17858f8336576c3575440
anisole16 <meerahulyalkar@gmail.com>
2026-06-11 20:43:04 +0530

    Add files via upload

---
+++ MyFolder/2.5.py
@@ -0,0 +1,6 @@
+a =10
+b = 10
+print(id(a) == id(b))
+
+# the output of the given program is True as program is to check that the id of the given value is true or false
+# by using comparision operator
